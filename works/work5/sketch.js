8a3dbd10502df54dfcbc7beb93d248bd118f129c
201920296 <114163954+201920296@users.noreply.github.com>
2022-12-23 13:43:22 +0900

    SSS

--- works/work5/sketch.js
+++ works/work5/sketch.js
@@ -1,42 +1,45 @@
-
-let theta;
+let e1, e2, e3;
 
 function setup() {
   let boundingRects = document
-    .getElementById("p5Canvas")
-    .getBoundingClientRect();
+  .getElementById("p5Canvas")
+  .getBoundingClientRect();
   let canvas = createCanvas(boundingRects.width, boundingRects.height);
   canvas.parent("p5Canvas");
-
+  noStroke();
+  e1 = new Eye(250, 16, 120);
+  e2 = new Eye(164, 185, 80);
+  e3 = new Eye(420, 230, 220);
 }
 
 function draw() {
-  background(0);
-  frameRate(30);
-  stroke(255);
-  let a = (mouseX / width) * 90;
-  theta = radians(a);
-  translate(width/2,height);
-  line(0,0,0,-120);
-  translate(0,-120);
-  branch(120);
-
+  background(102);
+  e1.update(mouseX, mouseY);
+  e2.update(mouseX, mouseY);
+  e3.update(mouseX, mouseY);
+  e1.display();
+  e2.display();
+  e3.display();
 }
 
-function branch(h) {
-  h *= 0.66;
-  if (h > 2) {
+function Eye(tx, ty, ts) {
+  this.x = tx;
+  this.y = ty;
+  this.size = ts;
+  this.angle = 0;
+
+  this.update = function(mx, my) {
+    this.angle = atan2(my - this.y, mx - this.x);
+  };
+
+  this.display = function() {
     push();
-    rotate(-theta);
-    line(0, 0, 0, -h);
-    translate(0, -h);
-    branch(h);
+    translate(this.x, this.y);
+    fill(255);
+    ellipse(0, 0, this.size, this.size);
+    rotate(this.angle);
+    fill(153, 204, 0);
+    ellipse(this.size / 4, 0, this.size / 2, this.size / 2);
     pop();
-    push();
-    rotate(theta);
-    line(0, 0, 0, -h);
-    translate(0, -h);
-    branch(h);
-    pop();
-  }
+  };
 }
